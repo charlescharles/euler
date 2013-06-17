125aa1728ea24ed326c913de2974f7ed163c3807
lightlike <cguo@princeton.edu>
2013-06-16 20:22:00 -0500

    more

--- smallest_multiple.py
+++
@@ -1,9 +0,0 @@
-def smallest_multiple(n):
-    left = [i for i in range(2, n+1)]
-    for x in range(n-1):
-        for y in range(x+1, n-1):
-            if left[y]%left[x] == 0:
-                left[y] /= left[x]
-    return left
-
-print reduce(lambda x,y: x*y, smallest_multiple(20))
--- square_difference.py
+++
@@ -1,4 +0,0 @@
-def sumsquares(n):
-    return reduce(lambda x,y:x+y, xrange(n))
-
-def squaresum(n):
